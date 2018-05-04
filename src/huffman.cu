ea99d7f0f18067a312f1051c027cbcaa9788ed8b
Vishal Shrivastava <vishal270692@gmail.com>
2018-05-04 04:07:56 -0700

    Decode parallelized

--- src/huffman.cu
+++ src/huffman.cu
@@ -57,14 +57,15 @@ void decode(Node* root, int &index, string str)
         decode(root->right, index, str);
 }
 
+//__device__ char* device_encode_map[TOTAL_CHARS * MAX_CODE_WIDTH];
 __constant__ char d_tree_arr [MAX_NODES];
 __global__ void encode_kernel(int k, int count,char *str,  char *encode_map)
 {
   if(threadIdx.x == 0){
     cudaStream_t s1, s2;
-    unsigned int flag = cudaStreamDefault;
-    cudaStreamCreateWithFlags(&s1, flag);
-    cudaStreamCreateWithFlags(&s2, flag);
+    //unsigned int flag = cudaStreamDefault;
+    cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+    cudaStreamCreateWithFlags(&s2, cudaStreamNonBlocking);
     //  printf("\nInside Kernel : %d and count : %d and string : %s",k, count, str);
     int l = 2*k+1;
     int r = 2*k+2;
@@ -76,8 +77,10 @@ __global__ void encode_kernel(int k, int count,char *str,  char *encode_map)
       #pragma unroll
       for ( ;i < MAX_CODE_WIDTH && str[i] != '\0'; i++) {
           encode_map[d_tree_arr[k]*MAX_CODE_WIDTH + i] = str[i];
+
       }
       encode_map[d_tree_arr[k]*MAX_CODE_WIDTH + i+1] = '\0';
+
 		  // printf("\nCHAR:%c ENCODEDING:%s",d_tree_arr[k], str);
       //fill_Map <<<1,1,0, s1>>>
 	  }
@@ -108,7 +111,56 @@ __global__ void encode_kernel(int k, int count,char *str,  char *encode_map)
   }
   __syncthreads();
 }
+__device__ int string_count = 0;
 
+
+
+__global__
+void decode_kernel(char *encoded_str,int k, int count) {
+  if(threadIdx.x == 0){
+    cudaStream_t s1, s2;
+    //unsigned int flag = cudaStreamDefault;
+    cudaStreamCreateWithFlags(&s1, cudaStreamNonBlocking);
+    cudaStreamCreateWithFlags(&s2, cudaStreamNonBlocking);
+    int l = 2*k+1;
+    int r = 2*k+2;
+    if (d_tree_arr[l] == '$' && d_tree_arr[r] == '$')
+    {
+        printf("\nDecoded CHAR:%c",d_tree_arr[k]);
+        string_count = count;
+    }
+    if (encoded_str[count] == '0' && d_tree_arr[l] != '$' ){
+      decode_kernel<<<1,1,0, s1>>>(encoded_str, l, count + 1);
+      cudaDeviceSynchronize();
+    }
+
+    if (encoded_str[count] == '1' && d_tree_arr[r] != '$') {
+      //printf("got right 1 count:%d\n", count);
+      //count++;
+
+      decode_kernel<<<1,1,0, s2>>>(encoded_str, r, count + 1);
+      cudaDeviceSynchronize();
+      //cudaStreamDestroy(s2);
+    }
+  }
+  __syncthreads();
+}
+
+/*__device__ void test(int cont){
+  printf("Test%d\n", count);
+  count++;
+}*/
+__global__
+void decode_parent_kernel(char *encoded_str, int string_size) {
+
+  if(threadIdx.x == 0){
+    while (string_count < string_size) {
+      decode_kernel<<<1,1,0>>>(encoded_str,0, string_count);
+      cudaDeviceSynchronize();
+    }
+  }
+  __syncthreads();
+}
 __global__ void my_concat_kernel(char *encode_table, char *input_string, char *encoded_string, size_t input_size)
 {
     //printf("In encode_kernel input string:%c\n", input_string[threadIdx.x]);
@@ -357,7 +409,7 @@ void buildHuffmanTree(string text)
         exit(EXIT_FAILURE);
     }
 
-#if 1
+#if 0
     printf("Final Encoded Table\n");
     //printf("\nCHAR:%c ENCODEDING:%s",h_arr[4], h_encode_map[97*MAX_CODE_WIDTH]);
     for (int i = 0; i < MAX_NODES ; i++) {
@@ -384,12 +436,67 @@ void buildHuffmanTree(string text)
     }
     cout << "\nEncoded string is :\n" << str << '\n';
 
-    // traverse the Huffman Tree again and this time
-    // decode the encoded string
-    int index = -1;
-    cout << "\nDecoded string is: \n";
-    while (index < (int)str.size() - 2) {
-        decode(root, index, str);
+
+    char h_input_string[str.length()];
+    strcpy(h_input_string, str.c_str());
+    char *d_input_string = NULL;
+    err = cudaMalloc((void**)&d_input_string, str.length()* sizeof(char));
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device input string (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
     }
-    cout << endl;
+    cudaMemset(d_input_string, '\0', str.length());
+    err = cudaMemcpy(d_input_string, h_input_string, str.length() * sizeof(char),cudaMemcpyHostToDevice);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy input string from host to device (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    char h_final_str[str.length()];
+    char *d_final_str = NULL;
+    err = cudaMalloc((void**)&d_final_str, text.length()* sizeof(char));
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to allocate device input string (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+  	cudaEventRecord(start);
+    decode_parent_kernel<<<blocksPerGrid, threadsPerBlock>>>(d_input_string, str.length());
+  	cudaEventRecord(stop);
+
+  	cudaEventSynchronize(stop);
+
+    elapsed = 0;
+  	cudaEventElapsedTime(&elapsed, start, stop);
+  	printf("The elapsed time for Encode kernal excution is %.2f ms\n", elapsed);
+
+    cudaEventDestroy (start);
+    cudaEventDestroy (stop);
+    cudaThreadSynchronize();
+
+    err = cudaMemcpy(h_final_str, d_final_str, text.length(), cudaMemcpyDeviceToHost);
+    if (err != cudaSuccess)
+    {
+        fprintf(stderr, "Failed to copy final string from device to host (error code %s)!\n", cudaGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    cudaThreadSynchronize();
+    cudaFree(d_str);
+    cudaFree(d_encode_map);
+    cudaFree(d_input_string);
+    cudaFree(d_final_str);
+  //  cudaFree(d_encode_table);
+    //cudaFree(d_encoded_string);
+
+
+    //free(h_encode_table);
+    //free(h_encoded_string);
+    free(h_arr);
+    free(h_encode_map);
+
 }
