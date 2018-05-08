e8a6efd98274808826575a2a5d4cf0db534a2aa7
Abhilash Tuse <abhilashtuse@gmail.com>
2018-05-08 15:09:28 -0700

    Add cpu side code

---
+++ cpu/cpu_huffman.cpp
@@ -0,0 +1,189 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
+#include <chrono>
+
+using namespace std;
+
+std::ofstream out_file("out.txt");;
+
+// A Tree node
+struct Node
+{
+    char ch;
+    int freq;
+    Node *left, *right;
+};
+
+// Function to allocate a new tree node
+Node* getNode(char ch, int freq, Node* left, Node* right)
+{
+    Node* node = new Node();
+
+    node->ch = ch;
+    node->freq = freq;
+    node->left = left;
+    node->right = right;
+
+    return node;
+}
+
+// Comparison object to be used to order the heap
+struct comp
+{
+    bool operator()(Node* l, Node* r)
+    {
+        // highest priority item has lowest frequency
+        return l->freq > r->freq;
+    }
+};
+
+// traverse the Huffman Tree and store Huffman Codes
+// in a map.
+void encode(Node* root, string str,
+            unordered_map<char, string> &huffmanCode)
+{
+    if (root == nullptr)
+        return;
+
+    // found a leaf node
+    if (!root->left && !root->right) {
+        huffmanCode[root->ch] = str;
+    }
+
+    encode(root->left, str + "0", huffmanCode);
+    encode(root->right, str + "1", huffmanCode);
+}
+
+// traverse the Huffman Tree and decode the encoded string
+void decode(Node* root, int &index, string str)
+{
+    if (root == nullptr) {
+        return;
+    }
+
+    // found a leaf node
+    if (!root->left && !root->right)
+    {
+        out_file << root->ch;
+        return;
+    }
+
+    index++;
+
+    if (str[index] =='0')
+        decode(root->left, index, str);
+    else
+        decode(root->right, index, str);
+}
+
+// Builds Huffman Tree and decode given input text
+void buildHuffmanTree(string text)
+{
+    // count frequency of appearance of each character
+    // and store it in a map
+    unordered_map<char, int> freq;
+    for (char ch: text) {
+        freq[ch]++;
+    }
+
+    // Create a priority queue to store live nodes of
+    // Huffman tree;
+    priority_queue<Node*, vector<Node*>, comp> pq;
+
+    // Create a leaf node for each character and add it
+    // to the priority queue.
+    for (auto pair: freq) {
+        pq.push(getNode(pair.first, pair.second, nullptr, nullptr));
+    }
+
+    // do till there is more than one node in the queue
+    while (pq.size() != 1)
+    {
+        // Remove the two nodes of highest priority
+        // (lowest frequency) from the queue
+        Node *left = pq.top(); pq.pop();
+        Node *right = pq.top();    pq.pop();
+
+        // Create a new internal node with these two nodes
+        // as children and with frequency equal to the sum
+        // of the two nodes' frequencies. Add the new node
+        // to the priority queue.
+        int sum = left->freq + right->freq;
+        pq.push(getNode('\0', sum, left, right));
+    }
+
+    // root stores pointer to root of Huffman Tree
+    Node* root = pq.top();
+
+    // traverse the Huffman Tree and store Huffman Codes
+    // in a map. Also prints them
+    unordered_map<char, string> huffmanCode;
+    encode(root, "", huffmanCode);
+
+    /*cout << "Huffman Codes are :\n" << '\n';
+    for (auto pair: huffmanCode) {
+        cout << pair.first << " " << pair.second << '\n';
+    }*/
+
+    //cout << "\nOriginal string was :\n" << text << '\n';
+
+    // print encoded string
+    string str = "";
+    for (char ch: text) {
+        str += huffmanCode[ch];
+    }
+
+    //cout << "\nEncoded string is :\n" << str << '\n';
+
+    // traverse the Huffman Tree again and this time
+    // decode the encoded string
+    int index = -1;
+    //cout << "\nDecoded string is: \n";
+    while (index < (int)str.size() - 2) {
+        decode(root, index, str);
+    }
+}
+
+// Huffman coding algorithm
+int main(int argc, char** argv)
+{
+    auto started = std::chrono::high_resolution_clock::now();
+
+    std::string inp_filename;
+	if(argc < 2) {
+        cerr << "Usage: " << argv[0] << " input_file_path" << endl;
+		return 1;
+	}
+	else
+    {
+        inp_filename = argv[1];
+    }
+
+	std::ifstream inp_file(inp_filename.c_str());
+	if (!inp_file.is_open()) {
+		cerr << "Error: Invalid input file: " << inp_filename << endl;
+		return 1;
+	}
+
+	if (!out_file.is_open()) {
+		cerr << "Error: could not create out.txt file: " << endl;
+		return 1;
+	}
+    std::stringstream buffer;
+
+    buffer << inp_file.rdbuf();
+    std::string text = buffer.str();
+    //string text = "Huffman";
+
+    buildHuffmanTree(text);
+    auto done = std::chrono::high_resolution_clock::now();
+    std::cout << "Total execution time:" << std::chrono::duration_cast<std::chrono::nanoseconds>(done-started).count();
+
+    inp_file.close();
+	out_file.close();
+    return 0;
+}
